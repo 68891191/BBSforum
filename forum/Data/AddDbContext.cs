f4902609a0c1b25094d434bdff5c9a72973b233b
68891191 <tc68891191@hotmail.com>
2023-07-29 20:21:17 +1200

    ASP.NET Core web application is utilizing Entity Framework Core for database access, Bootstrap for responsive styling, and jQuery Validation for client-side form validation.
    use SQLite as the database provider.

---
+++ forum/Data/AddDbContext.cs
@@ -0,0 +1,13 @@
+using forum.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace forum.Data;
+public class AddDbContext:DbContext
+{
+    protected AddDbContext(DbContextOptions options):base(options)
+    {
+
+    }
+
+    public DbSet<Users> Users{get;get;}
+}
---
+++ forum/Program.cs
@@ -0,0 +1,35 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using Forum.Data;
+using Microsoft .EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ForumDbContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("ForumDbContext") ?? throw new InvalidOperationException("Connection string 'ForumDbContext' not found.")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default time is 30 days.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
