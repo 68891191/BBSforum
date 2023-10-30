9aff1d81a4f44ae7deaa9dea185187aee1fb6acf
68891191 <tc68891191@hotmail.com>
2023-10-31 10:32:06 +1300

    the second Modification after final presentation 1. fix messages function.

--- forum/Controllers/MessageController.cs
+++ forum/Controllers/MessageController.cs
@@ -22,29 +22,23 @@ namespace forum.Controllers
         // GET: Message
         public async Task<IActionResult> Index()
         {
-            // Check if the user is authenticated
             if (HttpContext.Session.GetInt32("token") == null)
             {
                 return RedirectToAction("SignIn", "Auth");
             }
 
-            // Retrieve the user based on their session token
             var user = await _context.User.FirstOrDefaultAsync(u => u.token == HttpContext.Session.GetString("token"));
 
-            // If the user does not exist, return an error message
             if (user == null)
             {
                 return Problem("User does not exist.");
             }
 
-            // Retrieve and display sent and received messages for the user
             var sentMessages = _context.Message.Where(m => m.senderEmail == user.email).ToList().OrderByDescending(m => m.createdAt);
-            var receivedMessages = _context.Message.Where(m => m.receiverEmail == user.email).ToList().OrderByDescending(m => m.createdAt);
+            var recievedMessages = _context.Message.Where(m => m.receiverEmail == user.email).ToList().OrderByDescending(m => m.createdAt);
             ViewBag.sentMessages = sentMessages;
-            ViewBag.receivedMessages = receivedMessages;
-
-            // Return the view if the Message entity set is not null
-            return _context.Message != null ? View() : Problem("Entity set 'ForumDbContext.Message' is null.");
+            ViewBag.recievedMessages = recievedMessages;
+            return _context.Message != null ? View() : Problem("Entity set 'ForumDbContext.Message'  is null.");
         }
 
         // GET: Message/Create
@@ -54,31 +48,28 @@ namespace forum.Controllers
         }
 
         // POST: Message/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("id,senderEmail,receiverEmail,content")] Message message)
         {
-            // Check if the user is authenticated
             string? token = HttpContext.Session.GetString("token");
             if (token == null)
             {
                 return RedirectToAction("SignIn", "Auth");
             }
 
-            // Retrieve the user based on their session token
             var user = _context.User.FirstOrDefault(u => u.token == token);
 
-            // If the user does not exist, return an error
             if (user == null)
             {
                 return NotFound();
             }
 
-            // Set the sender's email to the user's email
             message.senderEmail = user.email;
 
-            // Check if sender and receiver email are provided, then add the message
-            if (!string.IsNullOrEmpty(message.senderEmail) && !string.IsNullOrEmpty(message.receiverEmail))
+            if (message.senderEmail != "" && message.receiverEmail != "")
             {
                 _context.Add(message);
                 await _context.SaveChangesAsync();
@@ -87,7 +78,6 @@ namespace forum.Controllers
             return View(message);
         }
 
-        // Check if a message with a specific id exists
         private bool MessageExists(int id)
         {
             return (_context.Message?.Any(e => e.id == id)).GetValueOrDefault();
--- forum/Models/Message.cs
+++ forum/Models/Message.cs
@@ -6,23 +6,20 @@ namespace Forum.Models;
 public class Message
 {
     [Key]
-    public int id { get; set; } // Unique identifier for the message.
-
+    public int id { get; set; }
+    
     [ForeignKey("User")]
     [EmailAddress]
     public string senderEmail { get; set; }
-    // Email address of the message sender, associated with the User entity.
-
+    
     [ForeignKey("User")]
     [EmailAddress]
     public string receiverEmail { get; set; }
-    // Email address of the message receiver, associated with the User entity.
-
+    
     public string content { get; set; } = default!;
-    // Content or body of the message.
-
+    
     [DataType(DataType.Date)]
     [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
     public DateTime createdAt { get; set; } = DateTime.Now;
-    // Date and time when the message was created, with specified display format.
+    
 }
